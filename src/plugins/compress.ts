c0d5bfc97ef1e9c854fb48afeb6e46ad8841d35a
GMKR <2089640+GMKR@users.noreply.github.com>
2021-05-22 10:14:02 +0530

    perf(core): tune br compression params to fit for API data

--- src/plugins/compress.ts
+++ src/plugins/compress.ts
@@ -1,8 +1,14 @@
 import FastifyPlugin from 'fastify-plugin';
 import FastifyCompress, { FastifyCompressOptions } from 'fastify-compress';
+import zlib from 'zlib';
 
 export default FastifyPlugin<FastifyCompressOptions>(async (fastify) => {
   fastify.register(FastifyCompress, {
     global: true,
+    brotliOptions: {
+      params: {
+        [zlib.constants.BROTLI_PARAM_MODE]: zlib.constants.BROTLI_MODE_TEXT,
+      },
+    },
   });
 });
